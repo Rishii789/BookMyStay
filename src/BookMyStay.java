4b08344edfa78f71d876cb61b3209dd7d6fd5747
Rishi789 <rishikumarsharma.789@gmail.com>
2026-03-16 14:26:51 +0530

    UC2: Basic Room Types

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,8 +1,59 @@
-public class BookMyStay {
+abstract class Room {
+    String type;
+    int beds;
+    int size;
+    double price;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+    Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
     }
 
+    void displayRoom(int availability) {
+        System.out.println(type + ":");
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+        System.out.println("Available: " + availability);
+        System.out.println();
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 750, 5000.0);
+    }
+}
+
+public class BookMyStay {
+    public static void main(String[] args) {
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        single.displayRoom(singleAvailability);
+        doub.displayRoom(doubleAvailability);
+        suite.displayRoom(suiteAvailability);
+    }
 }
